9dd2dbebb1c8621253650712a82c68fe326cdf9d
Mihai Rotaru <mihai.rotaru@gmx.com>
2018-08-07 06:35:57 +0100

    user error boundary component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux'
 
 import HomePage from './components/HomePage'
 import NotFoundPage from './components/NotFoundPage'
+import ErrorBoundary from './components/ErrorBoundary'
 import { routes as UsersRoutes } from './modules/users'
 import configureStore from './store'
 
@@ -11,20 +12,24 @@ import style from './style.css'
 
 const store = configureStore()
 
-const App = () => {
-  return (
-    <Provider store={store}>
-      <div class={style.main}>
-        <BrowserRouter>
-          <Switch>
-            <Route path="/" component={HomePage} exact />
-            <UsersRoutes />
-            <Route component={NotFoundPage} />
-          </Switch>
-        </BrowserRouter>
-      </div>
-    </Provider>
-  )
+class App extends React.Component {
+  render() {
+    return (
+      <ErrorBoundary>
+        <Provider store={store}>
+          <div class={style.main}>
+            <BrowserRouter>
+              <Switch>
+                <Route path="/" component={HomePage} exact />
+                <UsersRoutes />
+                <Route component={NotFoundPage} />
+              </Switch>
+            </BrowserRouter>
+          </div>
+        </Provider>
+      </ErrorBoundary>
+    )
+  }
 }
 
 export default App
---
+++ src/components/ErrorBoundary.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { hasError: false, error: null, info: null }
+  }
+
+  componentDidCatch(error, info) {
+    this.setState({
+      hasError: true,
+      error,
+      info,
+    })
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div>
+          <h1>Something went wrong.</h1>
+          <pre>{this.state.error.toString()}</pre>
+          <pre>{this.state.info.toString()}</pre>
+        </div>
+      )
+    }
+    return this.props.children
+  }
+}
