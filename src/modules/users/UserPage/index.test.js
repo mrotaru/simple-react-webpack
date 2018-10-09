a7b4817b2a9632a0f092e53347954d43f99d406b
Mihai Rotaru <mihai.rotaru@gmx.com>
2018-10-09 06:10:41 +0100

    test(users): snapshot test for UsersPage

--- src/modules/users/UserPage/index.test.js
+++ src/modules/users/UserPage/index.test.js
@@ -3,6 +3,6 @@ import React from 'react'
 import UserPage from '.'
 
 test('render a label', () => {
-  const wrapper = shallow(<UserPage />)
+  const wrapper = shallow(<UserPage match={{ params: { userId: 'foo' }}} />)
   expect(wrapper).toMatchSnapshot()
 })
